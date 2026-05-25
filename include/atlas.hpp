632302d75a2072ff03b075763f0f3495de4d6134
Jaime Redondo <jairegra@gmail.com>
2026-05-25 22:54:48 +0200

    Update alien sprites to be the same size

--- include/atlas.hpp
+++ include/atlas.hpp
@@ -39,30 +39,30 @@ namespace atlas {
     inline constexpr Rectangle sprites[Count] = {
         {0, 16, 13, 8},     // AlienExplode
         {22, 0, 6, 8},      // AlienShotExplode
-        {28, 0, 11, 8},     // Crab1
-        {39, 0, 11, 8},     // Crab2
-        {50, 0, 12, 8},     // Octopus1
-        {62, 0, 12, 8},     // Octopus2
-        {74, 0, 15, 8},     // PlayerExplode1
-        {89, 0, 16, 8},     // PlayerExplode2
-        {105, 0, 13, 8},    // PlayerNormal
-        {118, 0, 8, 8},     // PlayerShotExplode
-        {78, 16, 1, 4},     // PlayerShotNormal
-        {69, 16, 3, 6},     // PlungerShot1
-        {72, 16, 3, 6},     // PlungerShot2
-        {75, 16, 3, 6},     // PlungerShot3
-        {66, 16, 3, 6},     // PlungerShot4
-        {29, 16, 3, 7},     // RollingShot2
-        {32, 16, 3, 7},     // RollingShot4
-        {35, 16, 3, 7},     // RollingShot13
+        {28, 0, 12, 8},     // Crab1
+        {40, 0, 12, 8},     // Crab2
+        {52, 0, 12, 8},     // Octopus1
+        {64, 0, 12, 8},     // Octopus2
+        {76, 0, 15, 8},     // PlayerExplode1
+        {91, 0, 16, 8},     // PlayerExplode2
+        {107, 0, 13, 8},    // PlayerNormal
+        {120, 0, 8, 8},     // PlayerShotExplode
+        {86, 16, 1, 4},     // PlayerShotNormal
+        {77, 16, 3, 6},     // PlungerShot1
+        {80, 16, 3, 6},     // PlungerShot2
+        {83, 16, 3, 6},     // PlungerShot3
+        {74, 16, 3, 6},     // PlungerShot4
+        {37, 16, 3, 7},     // RollingShot2
+        {40, 16, 3, 7},     // RollingShot4
+        {43, 16, 3, 7},     // RollingShot13
         {0, 0, 22, 16},     // Shield
-        {13, 16, 8, 8},     // Squid1
-        {21, 16, 8, 8},     // Squid2
-        {38, 16, 3, 7},     // SquigglyShot1
-        {41, 16, 3, 7},     // SquigglyShot2
-        {44, 16, 3, 7},     // SquigglyShot3
-        {47, 16, 3, 7},     // SquigglyShot4
-        {50, 16, 16, 7},    // Ufo
+        {13, 16, 12, 8},    // Squid1
+        {25, 16, 12, 8},    // Squid2
+        {46, 16, 3, 7},     // SquigglyShot1
+        {49, 16, 3, 7},     // SquigglyShot2
+        {52, 16, 3, 7},     // SquigglyShot3
+        {55, 16, 3, 7},     // SquigglyShot4
+        {58, 16, 16, 7}     // Ufo
     };
 
     /**
